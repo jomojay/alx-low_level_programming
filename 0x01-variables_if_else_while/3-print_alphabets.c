f9a7206fffc0428ea9927325e715fe1c61f635d2
jomojay <djdrjay001@gmail.com>
2023-07-14 17:03:10 +0100

    betty errora correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@ int main(void)
 	char ch1 = 'a';
 	char ch2 = 'A';
 
-	while(ch1 <= 'z')
+	while (ch1 <= 'z')
 	{
 		putchar(ch1);
 		ch1++;
 	}
-	while(ch2 <= 'Z')
+	while (ch2 <= 'Z')
         {
-                putchar(ch2);
-                ch2++;
-        }
+		putchar(ch2);
+		ch2++;
+	}
 	putchar('\n');
 	return (0);
 }

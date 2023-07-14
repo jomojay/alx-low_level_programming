a41cd6cab1bdcb6ecf1d2a96f46300f0a077fc2b
jomojay <djdrjay001@gmail.com>
2023-07-14 17:23:59 +0100

    fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,22 @@
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+	int num1, num2;
 
-	for (i = 0; i <= 9; i++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (j = 0; j <= 8; j++)
+		for (num2 = num1; num2 <= 99; num2++)
 		{
-			for (k = j; k <= 9; k++)
-			{
-				for (l = (j + 1); l <= 9; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-					if (i <= 9 && j <= 8)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (num1 < 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}

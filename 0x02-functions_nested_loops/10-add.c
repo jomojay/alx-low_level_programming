18bd6759cfec0b65ac7f399964cbcd397abb42f5
jomojay <djdrjay001@gmail.com>
2023-07-18 18:47:07 +0100

    task 10 first comit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: Entry value1
+ * @b: Entry value2
+ * 
+ * Return: Result of addition (res)
+ */
+
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}

6d78c3d572f98fd0463f73075e39c1e3c730ee40
jomojay <djdrjay001@gmail.com>
2023-08-04 20:54:45 +0100

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to an array of string(s)
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error\n");
+	return (0);
+}

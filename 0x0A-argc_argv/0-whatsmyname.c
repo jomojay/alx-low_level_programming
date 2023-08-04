f54d530ca3776136e5d2ec36be7c6c74532e68a3
jomojay <djdrjay001@gmail.com>
2023-08-04 20:25:19 +0100

    correct betty error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,13 @@
 
 /**
  * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to an array of string(s)
  *
  * Return: Always 0 (success)
  */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[argc - 1]);
-	return(0);
+	return (0);
 }

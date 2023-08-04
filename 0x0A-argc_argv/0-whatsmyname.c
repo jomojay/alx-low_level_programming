c3267f041fc6472ee72aa2fb040463dc3c2d4491
jomojay <djdrjay001@gmail.com>
2023-08-04 20:21:48 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, arg
+
+This project repo teaches the usage of main() program arguments (argc and agrv).

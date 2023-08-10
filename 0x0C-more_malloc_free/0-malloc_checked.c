7562372d14a323e33450738698c57ec28f871fae
jomojay <djdrjay001@gmail.com>
2023-08-10 14:09:59 +0100

    1st trial for tak 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using call
+ * @b: size of the memory in bytes
+ *
+ * Return: Retunrs a void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+# 0x0C. C - More malloc, fre
+
+Continuation of the project of 0x0B malloc free

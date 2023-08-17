eb2abf7a7b3677ffd9b00c11d1b192005ef530c0
jomojay <djdrjay001@gmail.com>
2023-08-17 17:57:56 +0100

    commit for task 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings- prints strings followed by new line
+ * @separator: pointer to the separating string
+ * @n: argumrnt count
+ *
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *ptr;
+
+	if (separator == NULL)
+		separator = "";
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(ap, char *);
+		if (ptr == NULL)
+			printf("nil");
+		else
+			printf("%s", ptr);
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}

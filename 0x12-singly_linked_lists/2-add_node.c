977eb37df88732ecb63e58d4d37b70b8b053a0e7
jomojay <djdrjay001@gmail.com>
2023-08-24 21:49:17 +0100

    edge case

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = NULL;
 	size_t len = 0;
 
-	new = (list_t *)malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	while (str[len])

2159dd94e9cfa33d69bac45c6e0e6453ac381a32
jomojay <djdrjay001@gmail.com>
2023-08-29 22:21:48 +0100

    commit for task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a node at a given index
+ * @head: head node pointer
+ * @index: index of the given position
+ * Return: 1 (successful) ot 1 (failed)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp = NULL, *del = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = *head;
+	if (index == 0)                                 {
+		if ((*head)->next == NULL)
+			return (-1);
+		del = *head;
+		(*head) = (*head)->next;
+	}
+	else
+	{
+		while (i < (index - 1))
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		del = tmp->next;
+		tmp->next = del->next;
+	}
+	free(del);
+	return (1);
+}
